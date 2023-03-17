8ba48f51ac7247c8a6f77e4318d8d76ee2a8f79b
laivii <91199307+laivii@users.noreply.github.com>
2023-03-17 14:32:23 +0200

    guestbook

--- main.js
+++ main.js
@@ -2,19 +2,49 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
+function formatTable() {
+    var data = fs.readFileSync('./guestbook.json', 'utf8');
+    var tiedot = JSON.parse(data);
+
+    var table = `
+    <table id="guestbook">
+        <tr>
+            <th> Username </th>
+            <th> Country </th>
+            <th> Message </th>
+        </tr>`;
+
+    for(let i = 0; i < tiedot.length; i++){
+        var henkilö = tiedot[i];
+
+        table += `
+        <tr>
+            <td>${henkilö["username"]}</td>
+            <td>${henkilö["country"]}</td>
+            <td>${henkilö["message"]}</td>
+        </tr>`;
+    }
+
+    table += "</table>"
+
+    return table;
+}
+
+
 app.get('/', function(req, res) {
     var contents = fs.readFileSync('./public/index.html');
     res.send(contents.toString());
 });
 
 app.get('/guestbook', function(req, res) {
-    var contents = fs.readFileSync('./public/guestbook.html');
-    res.send(contents.toString());
+    var contents = fs.readFileSync('./public/guestbook.html', 'utf8');
+    var table = formatTable();
+    res.send(contents + table);
 });
 
 app.get('/newmessage', function(req, res) {
-    var contents = fs.readFileSync('./public/newmessage.html');
-    res.send(contents.toString());
+    var contents = fs.readFileSync('./public/newmessage.html', 'utf8');
+    res.send(contents);
 });
 
 app.get('/ajaxmessage', function(req, res) {
