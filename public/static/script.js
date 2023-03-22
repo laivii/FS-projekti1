29d375c0cabc4c3cc32a39f46f1226f78b76b917
laivii <91199307+laivii@users.noreply.github.com>
2023-03-22 17:29:56 +0200

    ajax

--- public/static/script.js
+++ public/static/script.js
@@ -1,1 +1,20 @@
 //Tänne kaikki frontti koodi ei main.js
+
+
+function ajax(){
+    var xhr = new XMLHttpRequest();
+    var url = "/ajaxmessage";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader();
+    xhr.onreadystatechange(function () {
+        document.getElementById().innerHTML = xhr.response;
+    });
+
+    var data = JSON.stringify({
+        "username" : username,
+        "country" : country,
+        "message" : message
+    });
+
+    xhr.send(data);
+}
